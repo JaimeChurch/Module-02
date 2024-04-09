e6c68aa9f32edc8f3607096a86795455e50184c8
Jaime Church <deesjaime@gmail.com>
2024-04-09 11:09:03 -0700

    After completing Module 02 video.

---
+++ test.js
@@ -0,0 +1,17 @@
+/* 3 Examples of basic arithmetic in JS */
+
+console.log(2 + 2);
+console.log(10 / 5);
+console.log(5 % 2);
+
+
+/* 2 Examples of declaring variables in JS */
+
+let a = 100;
+const x = 5;
+
+/* 2 Examples of using variables in arithmetic operations in JS */
+
+console.log(a + x);
+console.log(a / x);
+
